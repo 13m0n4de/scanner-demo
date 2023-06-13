8b74bd1676d419be97e2044fa7a2096e7e736299
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2023-06-14 00:41:28 +0800

    不使用函数获取 CONFIG

--- app/actors/__init__.py
+++ app/actors/__init__.py
@@ -1,16 +1,20 @@
+import logging
+
 import dramatiq
+
 from dramatiq.brokers.redis import RedisBroker
 
-from app import get_app_config
+from app.config import CONFIG
 
 
-REDIS_CONFIG = get_app_config()['redis']
+REDIS_CONFIG = CONFIG['redis']
 
 broker = RedisBroker(url=f"redis://{REDIS_CONFIG['host']}:{REDIS_CONFIG['port']}")
 broker.declare_queue("default")
 dramatiq.set_broker(broker)
 
-from app.actors.masscan import run_masscan
+
+from app.actors.scans import run_masscan
 
 __all__ = (
     "broker",
--- app/config.py
+++ app/config.py
@@ -1,12 +1,7 @@
 import os
 import rtoml
 
-from functools import lru_cache
 
-
-@lru_cache
-def get_app_config():
-    config_path = os.environ.get('CONFIG_PATH', 'config/config.toml')
-    with open(config_path) as f:
-        config = rtoml.load(f)
-    return config
+CONFIG_PATH = os.environ.get('CONFIG_PATH', 'config/config.toml')
+with open(CONFIG_PATH) as f:
+    CONFIG = rtoml.load(f)
