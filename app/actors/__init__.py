7c25003f876701955f4851d3d282015a9a3592a2
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2023-06-14 20:49:43 +0800

    添加 Worker 以及 用于存储结果的后端，使用另一容器运行

--- app/actors/__init__.py
+++ app/actors/__init__.py
@@ -1,17 +1,23 @@
-import logging
-
 import dramatiq
 
 from dramatiq.brokers.redis import RedisBroker
+from dramatiq.results import Results
+from dramatiq.results.backends import RedisBackend
+from dramatiq.worker import Worker
 
 from app.config import CONFIG
 
 
 REDIS_CONFIG = CONFIG['redis']
 
+backend = RedisBackend(url=f"redis://{REDIS_CONFIG['host']}:{REDIS_CONFIG['port']}")
+
 broker = RedisBroker(url=f"redis://{REDIS_CONFIG['host']}:{REDIS_CONFIG['port']}")
 broker.declare_queue("default")
+broker.add_middleware(Results(backend=backend))
 dramatiq.set_broker(broker)
+
+worker = Worker(broker=broker, queues="default")
 
 
 from app.actors.scans import run_masscan
