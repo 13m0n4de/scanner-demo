497875f5fc567b73e37b9d5e30de3012ca3ba7ee
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2023-06-14 20:50:35 +0800

    测试 actor 返回结果

--- app/actors/scans.py
+++ app/actors/scans.py
@@ -1,14 +1,11 @@
-import subprocess
-
 from typing import Dict
-from subprocess import CalledProcessError
 
 from app.actors import dramatiq
 from app.config import CONFIG
 
 
-@dramatiq.actor
-def run_masscan(config: Dict[str, str]):
+@dramatiq.actor(store_results=True)
+def run_masscan(config: Dict[str, str]) -> str:
     command = [
         part.format(
             binary=CONFIG['binary']['masscan'],
@@ -18,8 +15,4 @@ def run_masscan(config: Dict[str, str]):
             source_ip=config['source_ip'])
         for part in CONFIG['command']['masscan']]
     run_masscan.logger.info(command)
-    # try:
-    #     output = subprocess.check_output(command)
-    #     print(output)
-    # except CalledProcessError:
-    #     pass
+    return ''.join(command)
