4831358003b95d61068b976aa9f298cf34955186
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2023-06-14 22:07:21 +0800

    用命令行启动 dramatiq 不需要在代码里创建 worker

--- app/actors/__init__.py
+++ app/actors/__init__.py
@@ -3,7 +3,6 @@ import dramatiq
 from dramatiq.brokers.redis import RedisBroker
 from dramatiq.results import Results
 from dramatiq.results.backends import RedisBackend
-from dramatiq.worker import Worker
 
 from app.config import CONFIG
 
@@ -16,8 +15,6 @@ broker = RedisBroker(url=f"redis://{REDIS_CONFIG['host']}:{REDIS_CONFIG['port']}
 broker.declare_queue("default")
 broker.add_middleware(Results(backend=backend))
 dramatiq.set_broker(broker)
-
-worker = Worker(broker=broker, queues="default")
 
 
 from app.actors.scans import run_masscan
