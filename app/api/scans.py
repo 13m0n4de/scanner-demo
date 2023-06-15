529562524c9d15b1a8d6e7275479ae503a219c80
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2023-06-15 12:00:45 +0800

    添加路由获取 masscan 扫描任务状态和结果

--- app/api/scans.py
+++ app/api/scans.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from dramatiq.results import ResultMissing, ResultTimeout
 
 from app import models
 from app import actors
@@ -18,4 +19,24 @@ async def scan_by_masscan(config: models.MasscanConfig):
     return {
         "status": status,
         "message_id": message_id
+    }
+
+
+@router.get("/scans/masscan/{message_id}")
+async def get_masscan_status(message_id: str):
+    task_message = actors.run_masscan.message().copy(message_id=message_id)
+    try:
+        result = task_message.get_result()
+    except ResultMissing:
+        status = "missing"
+        result = None
+    except ResultTimeout:
+        status = "timeout"
+        result = None
+    else:
+        status = "completed"
+
+    return {
+        "status": status,
+        "result": result
     }
