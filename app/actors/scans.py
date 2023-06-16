0a7b7833c1c4450d0180ff1b441619f1965fef91
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2023-06-16 14:10:31 +0800

    删除日志记录

--- app/actors/scans.py
+++ app/actors/scans.py
@@ -9,10 +9,8 @@ from app.utils import build_command, parse_masscan_output
 @dramatiq.actor(store_results=True)
 def scan_by_masscan(params: Dict[str, str | int]) -> List[Dict[str, str | int]]:
     command = build_command('masscan', params)
-    # scan_by_masscan.logger.info(f'command => {" ".join(command)}')
 
     output = subprocess.check_output(command)
-    scan_by_masscan.logger.info(f'output => {output.decode()}')
 
     results = parse_masscan_output(output.decode())
     return results
