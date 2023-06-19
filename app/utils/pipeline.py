b5f7bba0350cb505a3bf13f83f819c208d7d652f
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2023-06-19 18:51:36 +0800

    完成 #2 使用 redis 作为 pipeline 存储后端

--- app/utils/pipeline.py
+++ app/utils/pipeline.py
@@ -1,13 +1,21 @@
 import uuid
 
+from redis import StrictRedis
 from dramatiq import Message, pipeline
 
-EXTERNAL_JOB_STORE = {}
+from app.config import CONFIG
+from app.utils import CustomJSONEncoder
 
 
 class StoredPipeline(pipeline):
-    def __init__(self, children=None, broker=None, job_id=None):
+    def __init__(self, children=None, broker=None, job_id=None, encoder=CustomJSONEncoder()):
         self.job_id = str(uuid.uuid4())
+        self.redis = StrictRedis(
+            host=CONFIG['redis']['host'],
+            port=CONFIG['redis']['port'],
+            db=CONFIG['redis']['db']
+        )
+        self.encoder = encoder
 
         if job_id:
             children = self.restore_pipeline(job_id)
@@ -16,26 +24,33 @@ class StoredPipeline(pipeline):
         super().__init__(children, broker=broker)
 
     def store_pipeline(self):
-        job = [msg.asdict() for msg in self.messages]
-        EXTERNAL_JOB_STORE[self.job_id] = job
+        pipeline_key = f"myapp:pipelines:{self.job_id}"
+
+        for message in self.messages:
+            message_data = self.encoder.encode(message.asdict())
+            self.redis.lpush(pipeline_key, message_data)
+
         return self.job_id
 
-    @staticmethod
-    def restore_pipeline(job_id):
+    def restore_pipeline(self, job_id):
+        pipeline_key = f"myapp:pipelines:{job_id}"
+        pipeline_data = self.redis.lrange(pipeline_key, 0, -1)
         messages = []
 
-        for message in EXTERNAL_JOB_STORE[job_id]:
-            messages.append(
-                Message(
-                    queue_name=message["queue_name"],
-                    actor_name=message["actor_name"],
-                    args=message["args"],
-                    kwargs=message["kwargs"],
-                    options=message["options"],
-                    message_id=message["message_id"],
-                    message_timestamp=message["message_timestamp"],
+        if pipeline_data:
+            for message in pipeline_data:
+                message_data = self.encoder.decode(message)
+                messages.append(
+                    Message(
+                        queue_name=message_data["queue_name"],
+                        actor_name=message_data["actor_name"],
+                        args=message_data["args"],
+                        kwargs=message_data["kwargs"],
+                        options=message_data["options"],
+                        message_id=message_data["message_id"],
+                        message_timestamp=message_data["message_timestamp"],
+                    )
                 )
-            )
 
         return messages
 
